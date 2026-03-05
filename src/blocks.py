fd8d512ebb92786585b91172174dabb952d94d1a
dice2005x3005 <marcoencino1@gmail.com>
2026-03-04 21:29:51 -0700

    added a blocktype enum and a function to assign a type to each block

--- src/blocks.py
+++ src/blocks.py
@@ -1,3 +1,14 @@
+from enum import Enum
+
+class BlockType(Enum):
+    PARAGRAPH = "paragraph"
+    HEADING = "heading"
+    CODE = "code"
+    QUOTE = "quote"
+    UNORDERED_LIST = "unordered_list"
+    ORDERED_LIST = "ordered list"
+
+
 def markdown_to_blocks(markdown):
     line = markdown.split("\n\n")
     new = []
@@ -6,3 +17,24 @@ def markdown_to_blocks(markdown):
             i.strip()
             new.append(i)
     return new
+
+def block_to_block_type(block):
+    lines = block.split("\n")
+    
+    if block.startswith(("# ", "## ", "### ", "#### ", "##### ", "###### ")):
+        return BlockType.HEADING
+    elif block.startswith("```\n") and block.endswith("```"):
+        return BlockType.CODE
+    elif block.startswith(">"):
+        return BlockType.QUOTE
+    elif block.startswith("- "):
+        return BlockType.UNORDERED_LIST
+    elif block.startswith("1. "):
+        i = 1
+        for line in lines:
+            if not line.startswith(f"{i}. "):
+                return BlockType.PARAGRAPH
+            i += 1
+        return BlockType.ORDERED_LIST
+    return BlockType.PARAGRAPH
+    
--- src/test_blocks.py
+++ src/test_blocks.py
@@ -1,4 +1,4 @@
-from blocks import markdown_to_blocks
+from blocks import markdown_to_blocks, block_to_block_type, BlockType
 
 def test_markdown_to_blocks(self):
             md = """
@@ -19,3 +19,17 @@ def test_markdown_to_blocks(self):
                     "- This is a list\n- with items",
                 ],
             )
+
+def test_block_to_block_types(self):
+        block = "# heading"
+        self.assertEqual(block_to_block_type(block), BlockType.HEADING)
+        block = "```\ncode\n```"
+        self.assertEqual(block_to_block_type(block), BlockType.CODE)
+        block = "> quote\n> more quote"
+        self.assertEqual(block_to_block_type(block), BlockType.QUOTE)
+        block = "- list\n- items"
+        self.assertEqual(block_to_block_type(block), BlockType.ULIST)
+        block = "1. list\n2. items"
+        self.assertEqual(block_to_block_type(block), BlockType.OLIST)
+        block = "paragraph"
+        self.assertEqual(block_to_block_type(block), BlockType.PARAGRAPH)
